0969f95081360b033c9e6478a1e458e158f146f9
RO ;P <robertogc99@hotmail.com>
2023-08-30 07:58:25 +0200

    Menos bugs

--- horario2trenes.py
+++ horario2trenes.py
@@ -3,17 +3,18 @@
 
 import json
 import copy
-from tqdm import tqdm
 import openpyxl
+from tqdm import tqdm
+from openpyxl.styles import Font
 from json.decoder import JSONDecodeError
 
 # ============ EN DESARROLLO ============
 
+# TODO : ACEPTAR LINEAS QUE A VECES SE SALTAN PARADAS (TIPO IC61)
+
 # ============ MÁS TARDE ============
 
-# TODO : VERIFICAR QUE TODO ESTE OK (CIUDADES)
-# TODO : NO COPAR EL PRIUMER TRAYECTO DE IDA Y SOLO INVERTIRLO (LIKE BRUH) POR LO MENOS BORRARLO, YA QUE LOS HORARIOS ESTAN MAL
-# TODO : ACEPTAR LINEAS QUE A VECES SE SALTAN PARADAS (TIPO IC61)
+# TODO : NO COPAR EL PRIMER TRAYECTO DE IDA Y SOLO INVERTIRLO (LIKE BRUH) POR LO MENOS BORRARLO, YA QUE LOS HORARIOS ESTAN MAL (creo que ya ha sido resuelto)
 
 # --------- CLASES ---------
 
@@ -106,25 +107,83 @@ def cargarTrenes():
 	
 	print(len(finaltrenes), "trenes cargados")
 
+def verificarCiudades(tener, tiene):	# Verificar que la lista "tiene" tiene todas las paradas en "tener" en el mismo orden
+	for i in range(len(tener)):
+		if (tener[0] != tiene[0]): return i
+		
+		del tener[0]
+		del tiene[0]
+	
+	return -1
+
+def crearLinea(i, ciuds):
+	print("Creando hoja para " + finaltrenes[i].nombre)
+	nuevoTren = workbook.create_sheet(title = finaltrenes[i].nombre)
+	
+	# Añadir el primer horario
+	nuevoTren.cell(row = 1, column = 1, value = 2)
+	nuevoTren.cell(row = 1, column = 2, value = 3)
+	nuevoTren.cell(row = 1, column = 3, value = 4)
+	nuevoTren.cell(row = 1, column = 4, value = 3 + len(ciuds))
+	
+	# Añadir el texto del tren
+	negrita = Font(bold = True, size = 10, name = "Arial")
+	
+	nuevoTren.merge_cells("A2:C2")
+	nuevoTren.cell(row = 2, column = 1, value = finaltrenes[i].nombre).font = negrita
+	
+	nuevoTren.cell(row = 3, column = 1, value = "Parada").font = negrita
+	nuevoTren.cell(row = 3, column = 2, value = "Llegada").font = negrita
+	nuevoTren.cell(row = 3, column = 3, value = "Salida").font = negrita
+	
+	# Añadir los datos del tren actual
+	for c in range(len(ciuds)):
+		nuevoTren.cell(row = c + 4, column = 1, value = ciuds[c]).font = negrita		# Añadir las paradas del tren
+		
+		nuevoTren.cell(row = c + 4, column = 2, value = finaltrenes[i].trayectos[0][2][c])	# Añadir el horario actual de llegada
+		nuevoTren.cell(row = c + 4, column = 3, value = finaltrenes[i].trayectos[0][3][c])	# Añadir el horario actual de salida
+	
+	# Añadir el texto de la vuelta
+	nuevoTren.cell(row = 1, column = 5, value = 2)
+	nuevoTren.cell(row = 1, column = 6, value = 2)
+	nuevoTren.cell(row = 1, column = 7, value = 9 + len(ciuds))
+	nuevoTren.cell(row = 1, column = 8, value = 8 + (2 * len(ciuds)))
+	
+	nuevoTren.merge_cells('A' + str(7 + len(ciuds)) + ":C" + str(7 + len(ciuds)))
+	nuevoTren.cell(row = 7 + len(ciuds), column = 1, value = finaltrenes[i].nombre).font = negrita
+	
+	nuevoTren.cell(row = 8 + len(ciuds), column = 1, value = "Parada").font = negrita
+	
+	# Añadir las paradas para el horario de vuelta
+	ciuds.reverse()
+	for c in range(len(ciuds)):	nuevoTren.cell(row = c + 9 + len(ciuds), column = 1, value = ciuds[c]).font = negrita # Añadir las paradas del tren
+	
+	workbook.save(horariosF)	# Guardar el archivo
+
 def procesarTrenes():
-	for i in tqdm(range(len(finaltrenes)), unit = "Trenes"): # Bucle que pasa por todos los trenes ya existentes
-		sheet = workbook[finaltrenes[i].nombre]
+	for i in tqdm(range(len(finaltrenes)), unit = "Trenes"):		# Bucle que pasa por todos los trenes ya existentes
+		horariosIda = copy.deepcopy(finaltrenes[i].trayectos[0])	# Coger el primer horario que deberia ser de ida
+		trenesEscribir.append(Tren(finaltrenes[i].id, finaltrenes[i].nombre, finaltrenes[i].color, [])) # Crear un trayecto vacio
+		trenesEscribir[i].trayectos.append(copy.deepcopy(horariosIda))
 		
-		if (sheet == None): continue # Si no existe una hoja con el nombre del tren -> ignorar (TODO : AÑADIR EL TREN AUNQUE NO ESTE EN EL EXCEL) <- the fuck he talking about
+		# Coger el nombre de las ciudades
+		ciudades = copy.deepcopy(horariosIda[0])
+		for c in range(len(ciudades)):
+			ciudades[c] = getCiudadfromId(ciudades[c]).nombre.upper()
 		
-		# Coger el primer horario que deberia ser de ida
-		horariosIda = copy.deepcopy(finaltrenes[i].trayectos[0])
+		# Coger la línea del tren desde el Excel si existe, si no, crearla
+		try:
+			sheet = workbook[finaltrenes[i].nombre]
+		except KeyError:
+			crearLinea(i, copy.deepcopy(ciudades))
+			sheet = workbook.create_sheet(finaltrenes[i].nombre)	# Crear la hoja si no existe
+			continue
 		
-		# Comprobar que las ciudades que pasa son las mismas que las del Excel
-		ciudades = horariosIda
 		
 		# Crear la base de los horarios de vuelta (lo mismo que de ida pero al reves)
 		horariosVuelta = copy.deepcopy(finaltrenes[i].trayectos[0])	# Los trayectos de vuelta son iguales que los de ida, excepto que se invierte el orden de las paradas TODO : DONT ? ESTO LO QUE HACE ES AÑADIR UN TRAYECTO CON EL *MISMO* HORARIO QUE EL PRIMER TRAYECTO DE IDA
 		horariosVuelta[0] = horariosVuelta[0][::-1] # Invertir las paradas
 		horariosVuelta[1] = horariosVuelta[1][::-1] # Invertir la ruta
-		
-		trenesEscribir.append(Tren(finaltrenes[i].id, finaltrenes[i].nombre, finaltrenes[i].color, [])) # Crear un trayecto vacio
-		trenesEscribir[i].trayectos.append(copy.deepcopy(horariosIda))
 		
 		curr = copy.deepcopy(horariosIda)
 		
@@ -134,7 +193,15 @@ def procesarTrenes():
 		start_row = sheet.cell(row = 1, column = 3).value
 		end_row = sheet.cell(row = 1, column = 4).value
 		
-		if (end_row + 1 - start_row != len(curr[2])): 	continue
+		# Verificar ciudades de ida
+		res = verificarCiudades(copy.deepcopy(ciudades), [str(sheet.cell(row = r, column = 1).value).upper() for r in range(start_row, end_row + 1)])
+		if (res != -1):
+			print("El nombre de las ciudades para la línea", sheet.title, "no concuerda. Se esperaba", ciudades[res], "se encontró", sheet.cell(row = start_row + res, column = 1).value)
+			continue
+		
+		if (end_row + 1 - start_row != len(curr[2])):
+			print(sheet.title, end_row + 1 - start_row , "paradas en el Excel, se esperaban", len(curr[2]), "paradas")
+			continue
 		
 		for col in range(start_col, end_col, 2):
 			tiempoLleg = []
@@ -156,11 +223,16 @@ def procesarTrenes():
 		
 		curr = copy.deepcopy(horariosVuelta)
 		
+		# Verificar ciudades de ida
+		ciudades.reverse()
+		res = verificarCiudades(copy.deepcopy(ciudades), [str(sheet.cell(row = r, column = 1).value).upper() for r in range(start_row, end_row + 1)])
+		if (res != -1):
+			print("El nombre de las ciudades para la línea", sheet.title, "no concuerda. Se esperaba", ciudades[res], "se encontró", sheet.cell(row = start_row + res, column = 1).value)
+			continue
+		
 		if (end_row + 1 - start_row != len(curr[3])):
 			print(sheet.title, end_row + 1 - start_row , "paradas en el Excel, se esperaban", len(curr[3]), "paradas")
 			continue
-		
-		print("Yendo de", openpyxl.utils.cell.get_column_letter(start_col), openpyxl.utils.cell.get_column_letter(end_col), start_row, end_row)
 		
 		for col in range(start_col, end_col, 2):
 			tiempoLleg = []
